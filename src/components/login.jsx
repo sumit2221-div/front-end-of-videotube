5e096f66021d1f4c9eb27cae4a1de768809fe771
sumit212-dev <132038818+sumit2221-div@users.noreply.github.com>
2024-05-23 10:57:06 +0530

    make some changes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -8,14 +8,13 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(''); // Reset error state
-    setLoading(true); // Set loading state to true
 
     try {
       // Basic validation
@@ -35,14 +34,16 @@ const Login = () => {
       if (response.data) {
         console.log(response.data);
         navigate("/");
-       
       }
     } catch (error) {
       console.error(error);
-      setError(error.message || 'An error occurred'); // Update error state with appropriate message
-    } finally {
-      setLoading(false); 
-     
+      // Check if the error has a response and a message
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message); // Set error from backend response
+      } else {
+        setError(error.message || 'An error occurred'); 
+        console.log(error)// Set generic error message
+      }
     }
   };
 
@@ -51,16 +52,6 @@ const Login = () => {
       <h1 className='mb-5 text-2xl font-bold text-center text-gray-800'>Login</h1>
 
       {error && <p className="mb-5 text-center text-red-500">{error}</p>}
-
-      {loading && (
-        <div className="flex items-center justify-center mb-5">
-          <svg className="w-6 h-6 mr-3 text-gray-600 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A8.001 8.001 0 0019.708 15H16v4.582zM20 12c0-4.418-3.582-8-8-8v4c2.22 0 4 1.79 4 4h4zm-2-1.709V7.001A7.97 7.97 0 0012 7v4.291l2.291 2.292z"></path>
-          </svg>
-          <span className="text-gray-600">Logging in...</span>
-        </div>
-      )}
 
       <input
         className="block w-full px-4 py-2 mb-4 bg-white border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
@@ -81,7 +72,6 @@ const Login = () => {
       <button
         className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
         type="submit"
-        disabled={loading}
       >
         Log In
       </button>
